9c13796c053171cc9d8d2359c3ec997f83158297
Luis Gomez <whoislewys@gmail.com>
2019-09-21 14:18:11 -0700

    working tempo calc

--- lib/screens/battle_screen/note_loader.dart
+++ lib/screens/battle_screen/note_loader.dart
@@ -21,18 +21,19 @@ TODO: i need to parse this track into
   ]
 ]
 */
-  // i should be getting 12 note on events. Good!
+  print('parsedmidi: ${parsedMidi.toString()}');
   int count = 0;
   parsedMidi.tracks[0].forEach((midiEvent) {
-    //  print('midi event tostr: ${midiEvent.toString()}');
+     print('midi event tostr: ${midiEvent.toString()}');
      if (midiEvent is NoteOnEvent) {
        count++;
-       print('noteonevent');
        print('midi event deltatime: ${midiEvent.deltaTime}');
+     }
+     if (midiEvent is SetTempoEvent) {
+       print('tempo: ${getBpmForMidiEvent(midiEvent)}');
      }
    });
      print('noteonevvents ct: $count');
-
   // List<NoteOnEvent> noteStarts = parsedMidi.tracks[0].where((midiEvent) {
   //    print('midi event tostr: ${midiEvent.toString()}');
   //    if (midiEvent is NoteOnEvent) {
@@ -49,4 +50,13 @@ TODO: i need to parse this track into
   // print('notestarts: $noteNumbers');
 
   return [[new Note(0, 12, 30)]];
+}
+
+/// Returns BPM/tempo given a MIDI SetTempoEvent
+double getBpmForMidiEvent(SetTempoEvent tempoEvent) {
+  // todo: for more generality, replace [oneMinuteInMicroseconds] with calculation of tempo using time signature and microsecondsperbeat
+  // info: https://stackoverflow.com/questions/2038313/converting-midi-ticks-to-actual-playback-seconds
+  // info: http://www.lastrayofhope.co.uk/2009/12/23/midi-delta-time-ticks-to-seconds/
+  int oneMinuteInMicroseconds = 60000000;
+  return oneMinuteInMicroseconds/tempoEvent.microsecondsPerBeat;
 }
