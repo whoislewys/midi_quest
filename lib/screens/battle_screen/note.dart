6894adfe7248141c6ca33a42bff4746c724ce2cf
idiscipulo <ikdiscipulo@gmail.com>
2019-09-20 21:47:00 -0700

    basic file structure

---
+++ lib/screens/battle_screen/note.dart
@@ -0,0 +1,3 @@
+/// This is the model for the notes that people can play in the game
+/// The game will load a dictionary where the keys is the start times of each midi note
+/// The values will be a list of these notes models
