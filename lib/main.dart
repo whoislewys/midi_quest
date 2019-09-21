e80747e47eb3dfcddc1cbc6c99d3386bb424c89d
Luis Gomez <whoislewys@gmail.com>
2019-09-21 00:16:34 -0700

    pubspec with overwrites, start note stuff, and add more traditional main method example

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,30 @@
 import 'package:flame/flame.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import 'my_game.dart';
 
 main() async {
-  Flame.initializeWidget();
+// MyGame game = new MyGame();
+
+Flame.initializeWidget();
+
+//   runApp(MaterialApp(
+//       home: Scaffold(
+//           body: Container(
+//     child: GameWrapper(game),
+//   ))));
+
+  // could also just do this
   runApp(new MyGame().widget);
 }
+
+// class GameWrapper extends StatelessWidget {
+//   final MyGame game;
+//   GameWrapper(this.game);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return game.widget;
+//   }
+// }
--- lib/screens/battle_screen/note.dart
+++ lib/screens/battle_screen/note.dart
@@ -1,3 +1,7 @@
 /// This is the model for the notes that people can play in the game
 /// The game will load a dictionary where the keys is the start times of each midi note
 /// The values will be a list of these notes models
+
+class Note {
+  
+}
